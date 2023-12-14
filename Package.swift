41991d815d10e68d58428da1a064a4e151641b07
guochenghao <chenghaoguo@mapxus.com>
2023-12-14 11:15:46 +0800

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "YYModel",
+  platforms: [
+    .macOS(.v10_10),
+    .iOS(.v9),
+    .tvOS(.v9),
+    .watchOS(.v2)
+  ],
+  products: [
+    .library(
+      name: "YYModel",
+      type: .dynamic,
+      targets: ["YYModel"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "YYModel",
+      path: "YYModel",
+      resources: [
+        .process("PrivacyInfo.xcprivacy")
+      ],
+      publicHeadersPath: "",
+      linkerSettings: [
+        .linkedFramework("Foundation"),
+        .linkedFramework("CoreFoundation")
+      ]
+    ),
+  ]
+)
